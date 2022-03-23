af2955c58b23873f8b8402ad17fe100cd906731e
ZanCoulibaly <58905348+ZanCoulibaly@users.noreply.github.com>
2022-03-23 16:25:58 +0000

    Upadted

--- src/main/java/com/example/apitutorat/ApiTutoratApplication.java
+++ src/main/java/com/example/apitutorat/ApiTutoratApplication.java
@@ -1,12 +1,16 @@
 package com.example.apitutorat;
 
 import com.example.apitutorat.admin.Administrateur;
+import com.example.apitutorat.admin.service.AdminServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiTutoratApplication implements CommandLineRunner {
+    @Autowired
+    AdminServiceImp adminServiceImp;
 
     public static void main(String[] args) {
         SpringApplication.run(ApiTutoratApplication.class, args);
@@ -15,5 +19,6 @@ public class ApiTutoratApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Administrateur administrateur = new Administrateur(null, "Coulibaly", "Zan", "balamine129", "azerty");
+        adminServiceImp.addAdmin(administrateur);
     }
 }
