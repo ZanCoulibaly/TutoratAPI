75111076131ee321f0632f3505b9746c27a6fff2
ZanCoulibaly <58905348+ZanCoulibaly@users.noreply.github.com>
2022-03-23 16:09:19 +0000

    admin par defaut

--- src/main/java/com/example/apitutorat/ApiTutoratApplication.java
+++ src/main/java/com/example/apitutorat/ApiTutoratApplication.java
@@ -1,13 +1,19 @@
 package com.example.apitutorat;
 
+import com.example.apitutorat.admin.Administrateur;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiTutoratApplication {
+public class ApiTutoratApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiTutoratApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Administrateur administrateur = new Administrateur(null, "Coulibaly", "Zan", "balamine129", "azerty");
+    }
 }
